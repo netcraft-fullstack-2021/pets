cd9f3899df61eba6a6faea9fccce492bfc8c4199
Gilad Solter <gilad.solter@gmail.com>
2022-04-10 22:12:26 +0300

    style and data

---
+++ src/CatsData.js
@@ -0,0 +1,29 @@
+const catsData = [
+    {
+        id: '1',
+        name: 'Jonny',
+        color: 'brown',
+        age: 12,
+        image: 'https://images.pexels.com/photos/1170986/pexels-photo-1170986.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },{
+        id: '2',
+        name: 'Mike',
+        color: 'orange',
+        age: 8,
+        image: 'https://images.pexels.com/photos/3777622/pexels-photo-3777622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },{
+        id: '3',
+        name: 'Will',
+        color: 'white',
+        age: 5,
+        image: 'https://images.pexels.com/photos/1314550/pexels-photo-1314550.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },{
+        id: '4',
+        name: 'David',
+        color: 'black',
+        age: 2,
+        image: 'https://images.pexels.com/photos/1573324/pexels-photo-1573324.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    }
+];
+
+export default catsData;
